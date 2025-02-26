7067ace23fa52bfe13a4a85935cb2e0124811079
informatique <informatique@gedal.fr>
2025-02-26 11:07:32 +0100

    Authentication page

--- museum-frontend/app/_layout.tsx
+++ museum-frontend/app/_layout.tsx
@@ -16,6 +16,8 @@ export default function RootLayout() {
   return (
     <>
       <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="auth" />
+        <Stack.Screen name="(tabs)" />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
